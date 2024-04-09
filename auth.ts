616c6fd2e417b5c88dd62abfc15e84c49782a5c6
DhanushPrabakaran <dhanushprabakaran18@gmail.com>
2024-04-09 21:11:13 +0530

    Update authentication and database configuration
    - Remove `User` model and related database functions
    - Add `@auth/prisma-adapter` and configure it with `PrismaClient`
    - Update `next-auth` configuration to use the new adapter
    - Remove `connectToDB` function and related dependencies
    - Add `@prisma/client` and `bson` as dependencies
    - Update `next.config.js` to allow image loading from Cloudinary
    
    Note: This commit also includes changes to `package-lock.json` due to package updates.

--- auth.ts
+++ auth.ts
@@ -1,49 +1,15 @@
+
 import NextAuth from "next-auth"
-import User from "@/models/user"
 import GitHub from "next-auth/providers/github"
 import Google from "next-auth/providers/google"
-import type { NextAuthConfig } from "next-auth"
-import { connectToDB } from "./utils/database"
+import { PrismaAdapter } from "@auth/prisma-adapter"
+import { PrismaClient } from "@prisma/client"
 
-export const config = {
-  theme: {
-    logo: "https://next-auth.js.org/img/logo/logo-sm.png",
-  },
+const prisma = new PrismaClient()
+
+export const { handlers, auth, signIn, signOut } = NextAuth({
+  adapter: PrismaAdapter(prisma),
   providers: [
-    GitHub,Google
+    Google,GitHub
   ],
-  basePath: "/auth",
-  callbacks: {
-    async signIn({ profile, account }) {
-      try {
-        await connectToDB();
-        const provider = account?.provider;
-        const email = profile?.email;
-        const user = await User.findOne({ email, provider });
-
-        if (!user) {
-          await User.create({
-            email,
-            provider,
-            username: profile?.name?.replace(" ", "").toLowerCase(),
-            image: profile?.picture || profile?.avatar_url, // Use conditional operator to handle missing 'picture'
-          });
-        }
-        return true;
-      } catch (error : any) {
-        console.error("Error: ", error.message);
-        return false;
-      }
-    },
-    jwt({ token, trigger, session }) {
-        if (trigger === "update") token.name = session.user.name
-        return token
-      },
-    async session({ session, token }: { session: any; token: any }) {
-      session.user.userId = token.userId;
-      return session;
-    },
-  },
-} satisfies NextAuthConfig
-
-export const { handlers, auth, signIn, signOut } = NextAuth(config)
+})
