33ce83b2eb63b5ce53824b37c0c258c297b86b01
DhanushPrabakaran <dhanushprabakaran18@gmail.com>
2024-04-09 19:23:24 +0530

    update

--- middleware.ts
+++ middleware.ts
@@ -1,31 +1,11 @@
-import { NextRequest, NextResponse } from "next/server";
-import { getToken } from "next-auth/jwt";
+export { auth as middleware } from "@/auth"
 
-const PROTECTED_API_ROUTES = ["/api/todo"];
-const PROTECTED_ROUTES = ["/dashboard"];
+// Or like this if you need to do something here.
+// export default auth((req) => {
+//   console.log(req.auth) //  { session: { user: { ... } } }
+// })
 
-export async function middleware(request: NextRequest) {
-  const isProtectedApiRoute = PROTECTED_API_ROUTES.some((route: string) => request.nextUrl?.pathname?.startsWith(route));
-  const isProtectedRoute = PROTECTED_ROUTES.some((route: string) => request.nextUrl?.pathname?.startsWith(route));
-
-  if (isProtectedApiRoute) {
-    const isAuth = await isAuthenticated(request);
-    if (!isAuth) {
-      return Response.json({ success: false, message: "Authentication failed" }, { status: 401 });
-    }
-  }
-
-  if (isProtectedRoute) {
-    const isAuth = await isAuthenticated(request);
-    if (!isAuth) {
-      return NextResponse.redirect(new URL("/", request.url));
-    }
-  }
-
-  return NextResponse.next();
+// Read more: https://nextjs.org/docs/app/building-your-application/routing/middleware#matcher
+export const config = {
+  matcher: ["/((?!api|_next/static|_next/image|favicon.ico).*)"],
 }
-
-const isAuthenticated = async (request: NextRequest) => {
-  const token: any = await getToken({ req: request });
-  return !!token && Date.now() <= token.exp * 1000;
-};
